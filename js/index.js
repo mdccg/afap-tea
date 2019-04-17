5d127d2c1854852968832f94a5d1992113addbb7
Matheus Daniel <matheusdanielgomess@gmail.com>
2019-04-16 20:53:00 -0400

    header e footer responsivos

--- js/index.js
+++ js/index.js
@@ -1,3 +1,22 @@
-window.onload = () => {
-    alert('js 1.6 funfando');
+function BkgdResize(tag, width1, height1) {
+    tag = document.querySelector(tag);
+    var width2 = tag.offsetWidth;
+    var height2 = (width2 * height1) / width1;
+    tag.style.height = height2 + 'px';
+    tag.style.backgroundRepeat = 'no-repeat';
+}
+
+function setResponsive() {
+    BkgdResize('header', 1200, 400);
+    BkgdResize('footer', 1146, 215);
+}
+
+var body = document.querySelector('body');
+
+body.onresize = function() {
+    setResponsive();
+}
+
+window.onload = function() {
+    setResponsive();
 }
