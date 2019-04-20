71a3784bfe1b7820e2b366c88d13083ec1b862ab
Matheus Daniel <matheusdanielgomess@gmail.com>
2019-04-20 17:21:11 -0400

    adicionado bordas nos elementos do menu de navegação

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
-function BkgdResize(tag, width1, height1) {
+function percent(n, per) { return (n * per) / 100; }
+
+function bkgdResize(tag, width1, height1) {
     tag = document.querySelector(tag);
     var width2 = tag.offsetWidth;
     var height2 = (width2 * height1) / width1;
@@ -7,8 +9,10 @@ function BkgdResize(tag, width1, height1) {
 }
 
 function setResponsive() {
-    BkgdResize('header', 1200, 400);
-    BkgdResize('footer', 1146, 215);
+    bkgdResize('header', 1200, 400);
+    bkgdResize('footer', 1146, 215);
+    bkgdResize('div.bkgd-ff0', 1146, percent(215, 15));
+    
 }
 
 var body = document.querySelector('body');
